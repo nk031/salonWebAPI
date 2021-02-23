13245c7eeae2ed98221e4fe7310d620ec15cfae9
Navjot Kaur <NK031@atcltd.atc.org.nz>
2021-02-23 14:48:44 +1300

    query

---
+++ salonWebAPI/Data/dbo.Client.data.sql
@@ -0,0 +1,7 @@
+﻿SET IDENTITY_INSERT [dbo].[Client] ON
+INSERT INTO [dbo].[Client] ([Id], [client_Name], [contact], [service], [arrived_Date]) VALUES (2, N'Navjot', N'34343', N'dfdfdf', N'2021-02-23 00:00:00')
+INSERT INTO [dbo].[Client] ([Id], [client_Name], [contact], [service], [arrived_Date]) VALUES (5, N'Lilly', N'0223456464', N'Eyebrow', N'2021-02-17 00:00:00')
+INSERT INTO [dbo].[Client] ([Id], [client_Name], [contact], [service], [arrived_Date]) VALUES (6, N'Blessy', N'0223455544', N'Wax', N'2021-02-22 00:00:00')
+INSERT INTO [dbo].[Client] ([Id], [client_Name], [contact], [service], [arrived_Date]) VALUES (7, N'Rose', N'0334322772', N'Tint', N'2021-02-19 00:00:00')
+INSERT INTO [dbo].[Client] ([Id], [client_Name], [contact], [service], [arrived_Date]) VALUES (8, N'Ammi', N'027898769', N'Facial', N'2021-02-26 00:00:00')
+SET IDENTITY_INSERT [dbo].[Client] OFF
